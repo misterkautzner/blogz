804c71b302cbc80c5a43d77e28f288a6251dd608
John Kautzner <misterkautzner@gmail.com>
2018-04-17 19:32:37 -0500

    Added login page.  Needs functionality

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blog@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -12,17 +12,33 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     body = db.Column(db.String(10000), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, user):
         self.title = title
         self.body = body
+        self.user = user
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(20))
+    blogs = db.relationship('Blog', backref='user')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 
 @app.route('/')
 def index():
     return redirect('/blog')
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+ 
 
 @app.route('/blog') 
 def blog():
@@ -64,7 +80,7 @@ def add():
         if title_error or body_error:
             return render_template('add_form.html', title='Add a Blog Entry', title_error = title_error, body_error = body_error, blog_title=title, body=body)       
 
-        new_blog_post = Blog(title, body)
+        new_blog_post = Blog(title, body) #, user)
 
         db.session.add(new_blog_post)
         db.session.commit()
